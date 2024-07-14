b26e971f35ccffc2ba946327ce0b7c66afd0cf2c
dlucioyauh <dlucio.douglas@gmail.com>
2024-07-14 15:07:02 -0300

    Desafio 18 - Verifique se todos os números da lista são iguais

---
+++ src/stream_api/Desafio18.java
@@ -0,0 +1,15 @@
+//Desafio 18 - Verifique se todos os números da lista são iguais
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio18 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        boolean todosIguais = numeros.stream()
+                .distinct()
+                .count() == 1;
+        System.out.println(todosIguais);
+    }
+}
