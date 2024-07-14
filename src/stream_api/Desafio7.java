6fd85d8490a7650f2a5cee7af4da6bccfaa089c5
dlucioyauh <dlucio.douglas@gmail.com>
2024-07-14 14:10:33 -0300

    Desafio 7 - Encontrar o segundo número maior da lista

---
+++ src/stream_api/Desafio7.java
@@ -0,0 +1,19 @@
+//Desafio 7 - Encontrar o segundo número maior da lista
+package stream_api;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Desafio7 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        Integer segundoMaior = numeros.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(null);
+        System.out.println(segundoMaior);
+    }
+}
