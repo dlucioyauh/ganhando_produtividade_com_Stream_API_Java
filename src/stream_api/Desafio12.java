fc2f2bbc4c3067b1fbd1d5803dd428d34087e212
dlucioyauh <dlucio.douglas@gmail.com>
2024-07-14 14:16:36 -0300

    Desafio 12 - Encontre o produto de todos os números da lista

---
+++ src/stream_api/Desafio12.java
@@ -0,0 +1,15 @@
+//Desafio 12 - Encontre o produto de todos os números da lista
+
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio12 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        int produto = numeros.stream()
+                .reduce(1, (a, b) -> a * b);
+        System.out.println(produto);
+    }
+}
