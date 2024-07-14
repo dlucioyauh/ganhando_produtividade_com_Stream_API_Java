2610769ace174fd8ca9b623f7d55f9f67e6110dc
dlucioyauh <dlucio.douglas@gmail.com>
2024-07-14 14:11:34 -0300

    Desafio 8 - Somar os dígitos de todos os números da lista

---
+++ src/stream_api/Desafio8.java
@@ -0,0 +1,15 @@
+//Desafio 8 - Somar os dígitos de todos os números da lista
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio8 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        int somaDigitos = numeros.stream()
+                .mapToInt(n -> String.valueOf(n).chars().map(Character::getNumericValue).sum())
+                .sum();
+        System.out.println(somaDigitos);
+    }
+}
